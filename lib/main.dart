3ae7a57eaf16a191ee3f7e6a2c96d2bfc910af53
nico-develop <contact@nico-develop.fr>
2021-12-08 17:27:34 +0100

    Fixed #20 - Intégration du tracker dans les settings

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:atrap_io/bootstrap_screen.dart';
 import 'package:atrap_io/repositories/authentication_repository.dart';
 import 'package:atrap_io/repositories/link_repository.dart';
+import 'package:atrap_io/repositories/tracker_repository.dart';
 import 'package:atrap_io/screens/get_tracker_screen.dart';
 import 'package:atrap_io/screens/login_screen.dart';
 import 'package:atrap_io/screens/register_screen.dart';
@@ -11,6 +12,7 @@ import 'package:atrap_io/services/add_link/add_link_bloc.dart';
 import 'package:atrap_io/services/app/app_bloc.dart';
 import 'package:atrap_io/services/auth_form/auth_form_bloc.dart';
 import 'package:atrap_io/services/delete_link/delete_link_bloc.dart';
+import 'package:atrap_io/services/generate_tracker/generate_tracker_bloc.dart';
 import 'package:atrap_io/services/list_links/list_links_bloc.dart';
 import 'package:atrap_io/services/login/login_bloc.dart';
 import 'package:atrap_io/services/logout/logout_bloc.dart';
@@ -79,6 +81,11 @@ class App extends StatelessWidget {
       functions: FirebaseFunctions.instance,
     );
 
+    TrackerRepository trackerRepository = TrackerRepository(
+      firestore: FirebaseFirestore.instance,
+      functions: FirebaseFunctions.instance,
+    );
+
     return MultiBlocProvider(
       providers: [
         BlocProvider<AppBloc>(
@@ -125,6 +132,12 @@ class App extends StatelessWidget {
         BlocProvider(
           create: (context) => UpdateLinkBloc(
             linkRepository: linkRepository,
+            authenticationRepository: authenticationRepository,
+          ),
+        ),
+        BlocProvider(
+          create: (context) => GenerateTrackerBloc(
+            trackerRepository: trackerRepository,
             authenticationRepository: authenticationRepository,
           ),
         )
