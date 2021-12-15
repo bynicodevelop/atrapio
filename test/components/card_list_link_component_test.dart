f24673e7de5eb1dbb59a46cf6793ea96697a8bc9
nico-develop <contact@nico-develop.fr>
2021-12-15 11:39:39 +0100

    Fixed #38 - Permet d'afficher le nombre de lien dans la liste des liens

--- test/components/card_list_link_component_test.dart
+++ test/components/card_list_link_component_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("Should check labels", (WidgetTester tester) async {
+  testWidgets("Should check labels (1)", (WidgetTester tester) async {
     final LinkModel link = LinkModel.fromJson(const {
       "uid": "uid",
       "src": "https://www.nico-develop.com",
@@ -21,5 +21,26 @@ void main() {
     expect(find.text('Clicks : 10'), findsOneWidget);
     expect(find.text('Nico Develop'), findsOneWidget);
     expect(find.text('https://www.nico-develop.com'), findsOneWidget);
+  });
+
+  testWidgets("Should check labels (2)", (WidgetTester tester) async {
+    final LinkModel link = LinkModel.fromJson(const {
+      "uid": "uid",
+      "src": "https://www.nico-develop.com/programme-formation-flutter",
+      "name": "Nico Develop - Web Site",
+      "clicks": 00,
+    });
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: CardListLinkComponent(link: link, actions: const []),
+      ),
+    ));
+
+    expect(find.text('Clicks : 0'), findsOneWidget);
+    expect(find.text('Nico Develop - Web Site'), findsOneWidget);
+    expect(
+        find.text('https://www.nico-develop.com/programme-formation-flutter'),
+        findsOneWidget);
   });
 }
